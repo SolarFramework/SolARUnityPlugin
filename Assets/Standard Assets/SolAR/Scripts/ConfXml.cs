4dd28aa7f3a0d6be2a076349ce9abd9b9c979292
BCOM\jroyan <jerome.royan@b-com.com>
2019-01-07 10:54:01 +0100

    Add a new Unity plugin to load SolAR pipelines

---
+++ Assets/Standard Assets/SolAR/Scripts/ConfXml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.ComponentModel;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace SolAR
+{
+    [Serializable]
+    [XmlRoot("xpcf-registry")]
+    public class ConfXml
+    {
+        [XmlElement("module")]
+        public Module[] modules;
+        [Serializable]
+        public class Module
+        {
+            [XmlAttribute]
+            public string uuid;
+            [XmlAttribute]
+            public string name;
+            [XmlAttribute]
+            public string path;
+            [XmlAttribute]
+            public string description;
+
+            [XmlElement("component")]
+            public Component[] components;
+            [Serializable]
+            public class Component
+            {
+                [XmlAttribute]
+                public string uuid;
+                [XmlAttribute]
+                public string name;
+                [XmlAttribute]
+                public string description;
+
+                [XmlElement("interface")]
+                public Interface[] interfaces;
+                [Serializable]
+                public class Interface
+                {
+                    [XmlAttribute]
+                    public string uuid;
+                    [XmlAttribute]
+                    public string name;
+                    [XmlAttribute]
+                    public string description;
+                }
+            }
+        }
+
+        public Configuration configuration;
+        [Serializable]
+        public class Configuration
+        {
+            [XmlElement("component")]
+            public ComponentConf[] components;
+            [Serializable]
+            public class ComponentConf
+            {
+                [XmlIgnore]
+                public string type;
+                [XmlAnyElement("Comment")]
+                public XmlComment Comment { get { return new XmlDocument().CreateComment(type); } set { } }
+
+                //[XmlAnyElement]
+                //public XmlElement[] elements = new XmlElement[2];
+
+                //[XmlAnyAttribute]
+                //public XmlAttribute[] attributes = new XmlAttribute[2];
+
+                [XmlAttribute]
+                public string uuid;
+                [XmlAttribute]
+                [DefaultValue("")]
+                public string name;
+
+                [XmlIgnore]
+                public string description;
+
+                [XmlElement("property")]
+                public Property[] properties;
+                [Serializable]
+                public class Property
+                {
+                    [XmlAttribute]
+                    public string name;
+                    [XmlAttribute]
+                    public string type;
+                    public enum TYPE
+                    {
+                        String,
+                        floaT,
+                        integeR,
+                        UnsignedIntegeR,
+                        Double,
+                    }
+                    [XmlAttribute]
+                    public string value;
+                    [XmlAttribute]
+                    [DefaultValue("")]
+                    public string description;
+
+                    [XmlElement("value")]
+                    public string[] values;
+                    public bool ShouldSerializevalues() { return values != null && values.Length > 0; }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Standard Assets/SolAR/Scripts/SolARPipeline.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using System.Runtime.InteropServices;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Xml.Linq;
+using System.Linq;
+using System.IO;
+
+namespace SolAR
+{
+
+    public class SolARPipeline : MonoBehaviour
+    {
+        public Camera m_camera;
+        private Canvas m_canvas;
+        private Texture2D m_texture;
+        private Material m_material;
+
+        [HideInInspector]
+        public string m_pipelineFolder;
+
+        [HideInInspector]
+        public string[] m_pipelinesName;
+
+        [HideInInspector]
+        public string[] m_pipelinesUUID;
+
+        [HideInInspector]
+        public string[] m_pipelinesPath;
+
+        [HideInInspector]
+        public int m_selectedPipeline;
+
+        [HideInInspector]
+        public string m_configurationPath;
+
+        [HideInInspector]
+        public string m_uuid;
+
+        public bool m_showDebugConsole = true;
+
+        [HideInInspector]
+        public ConfXml conf;
+
+        [HideInInspector]
+        public PipelineManager m_pipelineManager = new PipelineManager();
+
+        [DllImport("SolARPipelineManager")]
+        private static extern System.IntPtr RedirectIOToConsole(bool activate);
+        /*
+            [DllImport("SolARPipelineManager")]
+            private static extern System.IntPtr LogInFile([MarshalAs(UnmanagedType.LPStr)]string logFilePath, bool rewind);
+          */
+
+
+        void OnEnable()
+        {
+            // LogInFile("F:\\Dev\\SolAR\\sources\\Plugins\\SolARUnityPluginJR\\SolARPipelineManager.log", true);
+            if (m_showDebugConsole)
+                RedirectIOToConsole(true);
+            if (m_camera)
+            {
+                m_pipelineManager = new PipelineManager();
+                m_pipelineManager.init(m_configurationPath, m_uuid);
+                PipelineManager.CamParams camParams = m_pipelineManager.getCameraParameters();
+
+                GameObject goCanvas = new GameObject("VideoSeeThroughCanvas", typeof(Canvas), typeof(CanvasScaler), typeof(RawImage));
+                goCanvas.transform.SetParent(m_camera.transform);
+
+                m_texture = new Texture2D(camParams.width, camParams.height, TextureFormat.RGB24, false);
+                m_texture.filterMode = FilterMode.Point;
+                m_texture.Apply();
+
+                m_material = new Material(Shader.Find("Unlit/Texture"));
+                m_material.mainTexture = m_texture;
+
+                m_canvas = goCanvas.GetComponent<Canvas>();
+                m_canvas.worldCamera = m_camera;
+
+                CanvasScaler scaler = goCanvas.GetComponent<CanvasScaler>();
+                scaler.referenceResolution = new Vector2(camParams.width, camParams.height);
+                scaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+                scaler.screenMatchMode = CanvasScaler.ScreenMatchMode.Expand;
+                scaler.referencePixelsPerUnit = 1;
+
+                m_canvas.renderMode = RenderMode.ScreenSpaceCamera;
+                m_canvas.pixelPerfect = true;
+                m_canvas.worldCamera = Camera.main;
+                m_canvas.planeDistance = Camera.main.farClipPlane - 100;
+
+                RawImage image = goCanvas.GetComponent<RawImage>();
+                image.texture = m_texture;
+                image.material = m_material;
+                image.rectTransform.sizeDelta = new Vector2(camParams.width, camParams.height);
+
+                m_camera.fieldOfView = Mathf.Rad2Deg * 2 * Mathf.Atan(camParams.width / (2 * camParams.focalX)); 
+            }
+            else
+                Debug.Log("A canvas must be specified for the SolAR Pipeline component");
+        }
+
+        void OnDisable()
+        {
+            m_pipelineManager.Dispose();
+            if (m_showDebugConsole)
+                RedirectIOToConsole(false);
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            if (m_texture != null)
+                m_pipelineManager.start(m_texture.GetNativeTexturePtr());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            PipelineManager.Pose pose = new PipelineManager.Pose();
+            m_pipelineManager.udpate(pose);
+            float toto = pose.translation(0);
+            // Debug.Log("Translation = " + pose.T.ToString());
+            // Debug.Log("Rotation = " + pose.R.ToString());
+        }
+    }
+
+}
