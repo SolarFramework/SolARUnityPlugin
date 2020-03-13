375efcae389d2a8068c0939c92e1a076b4f0a191
BCOM\jroyan <jerome.royan@b-com.com>
2020-03-13 14:46:57 +0100

    Install third parties before bundle

--- Bundle.bat
+++ Bundle.bat
@@ -4,6 +4,14 @@ SET SOLAR_PIPELINE_MANAGER_VERSION=0.7.0
 SET SOLAR_WRAPPER_VERSION=0.7.0
 echo off
 
+echo Install third parties
+:: install all third parties in the ./Assets/plugins folder based on the packagedependencies_expert.txt file. More information on remaken is available on https://github.com/b-com-software-basis/remaken 
+remaken install -c %config% --cpp-std 17 -b cl-14.1 packagedependencies_Expert.txt
+
+echo Bundle third parties
+:: Bunlde all third parties in the ./Assets/plugins folder based on the packagedependencies_expert.txt file. More information on remaken is available on https://github.com/b-com-software-basis/remaken 
+remaken bundle file packagedependencies_Expert.txt -d ./Assets/Plugins -c %config% --cpp-std 17 -b cl-14.1
+
 echo Delete following pipeline manager wrapper files ?
 
 del ".\Assets\SolAR\Swig\*.*" /S /Q
@@ -21,7 +29,6 @@ xcopy "%REMAKEN_PKG_ROOT%\packages\SolARBuild\%compiler%\SolARWrapper\%SOLAR_WRA
 xcopy "%REMAKEN_PKG_ROOT%\packages\SolARBuild\%compiler%\SolARWrapper\%SOLAR_WRAPPER_VERSION%\csharp\SolAR\Datastructure\solar_datastructurePINVOKE.cs" ".\Assets\SolAR\Swig\Utilities\" /S
 xcopy "%REMAKEN_PKG_ROOT%\packages\SolARBuild\%compiler%\SolARWrapper\%SOLAR_WRAPPER_VERSION%\csharp\SolAR\Datastructure\solar_datastructure.cs" ".\Assets\SolAR\Swig\Utilities\" /S
 
-:: Bunlde all third parties in the ./Assets/plugins folder based on the packagedependencies_expert.txt file. More information on remaken is available on https://github.com/b-com-software-basis/remaken 
-remaken bundle file packagedependencies_Expert.txt -d ./Assets/Plugins -c %config% --cpp-std 17 -b cl-14.1
+
 
 exit /B 0
