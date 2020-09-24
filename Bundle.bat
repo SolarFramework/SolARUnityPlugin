8c4243f21fece82b33589dd467e8947fc3a631b7
Vincent Berthet <vincent.berthet@b-com.com>
2020-09-24 16:35:31 +0200

    Add Android to bundle.bat

--- Bundle.bat
+++ Bundle.bat
@@ -8,8 +8,8 @@ if "%~1"=="" (SET bconfig=release) else (SET bconfig=%1)
 echo Bundle third parties : %bconfig%
 :: Bunlde all third parties in the ./Assets/plugins folder based on the packagedependencies.txt file. More information on remaken is available on https://github.com/b-com-software-basis/remaken 
 remaken bundle -d ./Assets/Plugins -c %bconfig% --cpp-std 17 -b cl-14.1 packagedependencies.txt
-::remaken bundle -d ./Assets/Plugins/Android -c %bconfig% --cpp-std 17 -b clang -o android -a arm64-v8a packagedependencies.txt 
-::remaken bundle -d ./Assets/Plugins/Android -c %bconfig% --cpp-std 17 -b clang -o android -a arm64-v8a packagedependencies-android.txt 
+remaken bundle -d ./Assets/Plugins/Android -c %bconfig% --cpp-std 17 -b clang -o android -a arm64-v8a packagedependencies.txt 
+remaken bundle -d ./Assets/Plugins/Android -c %bconfig% --cpp-std 17 -b clang -o android -a arm64-v8a packagedependencies-android.txt 
 
 ::wrap cpp to c# interfaces
 echo ---------------- wrap files with SWIG ----------------------
